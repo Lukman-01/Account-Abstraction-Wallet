4aa5d1af3771587af1b197ec52f9e10490e3febb
Lukman-01 <abdulyekeenlukman@gmail.com>
2023-12-30 11:37:39 +0100

    react setup

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Links from '../src/components/ Links';
+import 'tailwindcss/tailwind.css';
+
+const App = () => {
+
+  return (
+    <div>
+      <Links />
+    </div>
+  );
+};
+
+export default App;
